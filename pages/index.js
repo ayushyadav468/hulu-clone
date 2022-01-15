59a5ca8442ed8fa291c95261d694d85d93f08daa
Ayush Yadav <ayushyadav468@gmail.com>
2022-01-15 16:22:54 +0530

    Thumbnail component added

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Head from 'next/head';
+
 import Header from '../components/Header';
 import Navbar from '../components/Navbar';
+import Results from '../components/Results';
 
-export default function Home() {
+import requests from '../utils/requests';
+
+export default function Home({ results }) {
 	return (
 		<div>
 			<Head>
@@ -12,7 +16,22 @@ export default function Home() {
 
 			<Header />
 			<Navbar />
-			{/* <Results/> */}
+			<Results results={results} />
 		</div>
 	);
+}
+
+export async function getServerSideProps(context) {
+	const genre = context.query.genre;
+	const request = await fetch(
+		`https://api.themoviedb.org/3${
+			requests[genre]?.url || requests.fetchTrending.url
+		}`
+	).then((res) => res.json());
+
+	return {
+		props: {
+			results: request.results
+		}
+	};
 }
